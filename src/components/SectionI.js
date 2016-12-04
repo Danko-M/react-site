f09e9e2e470327cb95d0111f87bec41e846219a8
Danko-M <dankomilutinovic@gmail.com>
2016-12-04 13:36:19 +0100

    changed carousel controls (SectionI Component)

--- src/components/SectionI.js
+++ src/components/SectionI.js
@@ -6,7 +6,11 @@ export const SectionI  = () =>
 		<Grid>
 	    <Row>
 	    	<h3 className="text-uppercase dashed-underline">Client's Testimonials</h3>
-      	<Carousel indicators={false} >
+      	<Carousel 
+      		indicators={false} 
+					prevIcon={<i className="fa fa-angle-left fa-3x" aria-hidden="true"></i>} 
+					nextIcon={<i className="fa fa-angle-right fa-3x" aria-hidden="true"></i>} 
+				>
 			    <Carousel.Item>
 		        <h3>First slide label</h3>
 		        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
